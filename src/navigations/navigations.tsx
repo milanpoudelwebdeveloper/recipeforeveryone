5a9c493108a9295ed3567cd3267ffabba8da22f8
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-05-21 08:30:09 +0545

    Add navigations and stack using react native navigation and related packages

---
+++ src/navigations/navigations.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
